694a1ca6bf40f9714069a4048d3fc490089442cb
kubanychhbekova <a7966519@gmail.com>
2023-06-21 15:03:05 +0600

    add footer to app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import "./style.scss"
 import Header from "./components/header/header";
 import Pages from "./components/pages/pages";
+import Footer from "./components/footer/footer";
 
 function App ()  {
     return (
         <div>
           <Header/>
 <Pages/>
+            <Footer/>
         </div>
     );
 };
