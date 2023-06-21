9efd69ffb08fb0958252bb2864bfab6c7b73a66c
kanjar45 <kenovkenn@gmail.com>
2023-06-21 14:51:10 +0600

    footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className='container'>
+                <div className='footer--block'>
+                    <h1>Sign up for our newsletter</h1>
+                   <p>Be the first to know about our special offers, news, and updates.</p>
+                  <input type="email"/>
+                <div/>
+                <div className='footer-title'>
+                    <h4>Lorem Ipsum</h4>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                </div>
+                    <div className='footer-title'>
+                <h4>Lorem Ipsum</h4>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                </div>
+                    <div className='footer-title'>
+                <h4>Lorem Ipsum</h4>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                </div>
+                    <div className='footer-title'>
+                <h4>Lorem Ipsum</h4>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                </div>
+            </div>
+
+        </div>
+            </div>
+
+
+
+    );
+};
+
+export default Footer;
