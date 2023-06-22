fa50487c584ddca3a2d3ce1b0f1df9d8d020a24a
kubanychhbekova <a7966519@gmail.com>
2023-06-22 13:43:46 +0600

    router dom

---
+++ src/components/allBooks/allBooks.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AllBooks = () => {
+    return (
+        <div id="books">
+            <div className="books">
+                <div className="books--title">
+                    <h1></h1>
+                </div>
+                <div className="books--center"></div>
+                <div className="books--bottom"></div>
+            </div>
+        </div>
+    );
+};
+
+export default AllBooks;
